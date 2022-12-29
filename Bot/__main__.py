53c3360ec6c8915ae4491977d3591e52348961d3
HMTD-Movies <111636041+HMTD-Movies@users.noreply.github.com>
2022-12-29 07:15:35 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, idle
 import os
 from Bot.video_stream import app
-API_ID = os.environ.get("API_ID",12345)
-API_HASH = os.environ.get("API_HASH","")
-TOKEN = os.environ.get("TOKEN","")
+API_ID = os.environ.get("API_ID",11973721)
+API_HASH = os.environ.get("API_HASH","5264bf4663e9159565603522f58d3c18")
+TOKEN = os.environ.get("TOKEN","5933906749:AAFgwZRU4tWlnaxRoJm9VW1n451RB7PoBQs")
 
 bot = Client(
     ":memory:",
